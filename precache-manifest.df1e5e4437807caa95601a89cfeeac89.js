73885d5264c9e6232856357c702697179cf886b2
Vipansh Thakur <vipansh@github.com>
2020-10-10 21:32:46 +0530

    Updates

---
+++ precache-manifest.df1e5e4437807caa95601a89cfeeac89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d25521d83f9a5a6a700302936683e10",
+    "url": "/two-dice/index.html"
+  },
+  {
+    "revision": "e2df5c95d57f1b9c7d2c",
+    "url": "/two-dice/static/css/main.99836fd6.chunk.css"
+  },
+  {
+    "revision": "fa5b5befe77ca207f342",
+    "url": "/two-dice/static/js/2.f957d51d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/two-dice/static/js/2.f957d51d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2df5c95d57f1b9c7d2c",
+    "url": "/two-dice/static/js/main.55d16708.chunk.js"
+  },
+  {
+    "revision": "c8574c9aaf22f8944d70",
+    "url": "/two-dice/static/js/runtime-main.c2f704fd.js"
+  }
+]);
